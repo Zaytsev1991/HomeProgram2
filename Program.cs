2a49586b04a4f1d91bc225c7e8a57e18c9591380
Evgeniy <avto8061@mail.ru>
2022-10-20 14:04:42 +0300

    HomeW2

--- Program.cs
+++ Program.cs
@@ -1,53 +1,9 @@
-﻿//Задача 10
-//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿//Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 /*
-int SecondNumber(int number)
+bool Palindrom(int n)
 {
-    int twodigit = number / 10;
-    int result = twodigit % 10;
-    return result;
-}
-
-Console.Write("threedigit number = ");
-int num = Convert.ToInt32(Console.ReadLine());
-
-int second = SecondNumber(num);
-
-Console.WriteLine("The Second Number " + second);
-*/
-
-
-//Задача 13
-//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
-/*
-int ThirdNumber(int number)
-{
-    int threedigit = number / 100;
-    int result = threedigit % 10;
-    return result;
-}
- 
-Console.Write("Push number = ");
-int num = Convert.ToInt32(Console.ReadLine());
-
-if (num > 99)
-{
-    num = ThirdNumber(num);
-    Console.WriteLine("Third Number = " + num);
-}
-else
-{
-    Console.WriteLine("We haven't third number");
-}
-*/
-
-//Задача 15
-//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
-
-/*
-bool WeekNumber(int n)
-{
-    if (n == 6 || n == 7)
+    if (n % 10 == n / 10000 && (n / 10) % 10 == (n / 1000) % 10)
     {
         return true;
     }
@@ -57,8 +13,59 @@ bool WeekNumber(int n)
     }
 }
 
-Console.Write("Push number of the day of the week  ");
+Console.Write("Push 5digit number  ");
 int day = Convert.ToInt32(Console.ReadLine());
-bool res = WeekNumber(day);
-Console.Write("Is it a day off?  " + res);
+bool res = Palindrom(day);
+Console.Write("Is it a palindrom?  " + res);
+*/
+
+//Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+/*
+double Lenght(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double lenght = Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y2-y1,2) + Math.Pow(z2-z1,2));
+    return lenght;
+}
+Console.Write("x1 = ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y1 = ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z1 = ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("x2 = ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y2 = ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z2 = ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double lenght = Lenght(x1, y1, z1, x2, y2, z2);
+Console.WriteLine("lenght = " + Math.Round(lenght,3));
+*/
+
+//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+/*
+void Cube(int n)
+{
+    int current = 1;
+    if (n < 1)
+    {
+        Console.WriteLine("Thank you, Mario! But your cubes are in another castle. Try something else NATURAL N");
+    }
+    else
+    {
+        while(current <= n)
+        {
+            Console.Write(Math.Pow(current,3) + " ");
+            current++;
+        }
+    }
+}
+
+Console.Write("n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Cube(n);
 */
